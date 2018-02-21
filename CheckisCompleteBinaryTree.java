2c7d082aa7637b82b093ecd1b4d8ac68e4794b81
liuyisi <yisi.liu17@gmail.com>
2018-02-20 21:35:19 -0600

    Laicode Pass

--- CheckisCompleteBinaryTree.java
+++ CheckisCompleteBinaryTree.java
@@ -1,7 +1,40 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 /**
  * Created by liuyisi on 2018/2/19.
  */
 public class CheckisCompleteBinaryTree {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isCompleted(TreeNode root) {
+        if (root == null)   return true;
+        //Queue initialization
+        Queue<TreeNode> queue = new LinkedList<>();
+        // if the flag is set true, there should not be any child children afterwards
+        boolean flag = false;
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur = queue.poll();
+            if (cur.left == null) {
+                flag = true;
+            } else if (flag) {
+                return false;
+            } else {
+                queue.offer(cur.left);
+            }
+
+            if (cur.right == null) {
+                flag = true;
+            } else if (flag) {
+                return false;
+            } else {
+                queue.offer(cur.right);
+            }
+        }
+        return true;
+    }
 }
