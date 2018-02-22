fc9684c6d8ff3af3394179db1c3f60a4dc0a595b
liuyisi <yisi.liu17@gmail.com>
2018-02-22 00:42:13 -0600

    98 Accepted

--- IsBST.java
+++ IsBST.java
@@ -1,7 +1,35 @@
-package PACKAGE_NAME;
-
 /**
  * Created by liuyisi on 2018/2/21.
  */
 public class IsBST {
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    /**
+     * Classic Problem : corner case: root == null
+     *                  Children node must strictly smaller or larger than root value
+     * @param root
+     * @return
+     */
+    public boolean isValidBST(TreeNode root) {
+        if (root == null)   return true;
+        long max = Long.MAX_VALUE;
+        long min = Long.MIN_VALUE;
+
+        return helper(root.left, min, root.val) && helper(root.right, root.val, max);
+    }
+    private boolean helper (TreeNode root, long min, long max) {
+        if (root == null) return true;
+        boolean ans = root.val > min && root.val < max ? true : false;
+        if (ans == false)   return false;
+        else {
+            boolean left = helper(root.left, min, root.val);
+            boolean right = helper(root.right, root.val, max);
+            return left && right;
+        }
+    }
 }
---
+++ MoveZeros.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by liuyisi on 2018/2/22.
+ */
+public class MoveZeros {
+}
