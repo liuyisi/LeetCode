681c37d8ef920709a99069df576c99255c7a890d
liuyisi <yisi.liu17@gmail.com>
2018-03-01 14:33:59 -0600

    17 Accepted

---
+++ HashMap.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by liuyisi on 2018/2/27.
+ */
+public class HashMap {
+}
--- LetterCombinationofPhoneNumber.java
+++ LetterCombinationofPhoneNumber.java
@@ -1,7 +1,31 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 /**
  * Created by liuyisi on 2018/2/23.
  */
 public class LetterCombinationofPhoneNumber {
+    private static final String[] KEYS = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+    public List<String> letterCombinations (String digits) {
+        List<String> ans = new ArrayList<String>();
+        if (digits == null || digits.length() == 0) {
+            ans.add("");
+            return ans;
+        }
+        StringBuilder sb = new StringBuilder();
+        helper(digits, 0, ans, sb);
+        return ans;
+    }
+    private void helper (String digits, int index, List<String> ans, StringBuilder prefix) {
+        if (index == digits.length()) {
+            String s = prefix.toString();
+            ans.add(s);
+            return;
+        }
+        int num = digits.charAt(index) - '0';
+        for (int i = 0; i < KEYS[num].length(); i++) {
+            prefix.append(KEYS[num].charAt(i));
+            helper(digits, index + 1, ans, prefix);
+            prefix.deleteCharAt(prefix.length() - 1);
+        }
+    }
 }
